6755e5fb07c4a570c4f2807dc66e1bd9c39c6af2
gimtaehyun <wereter31@naver.com>
2022-09-17 12:30:13 +0900

    Simplify code

--- app.py
+++ app.py
@@ -1,30 +1,3 @@
-# from flask import Flask, request, render_template
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def hello():
-#     return 'Hello, World!'
-#
-# @app.route('/hello')
-# def hellohtml():
-#     return render_template("hello.html")
-#
-# @app.route('/method', methods=['GET', 'POST'])
-# def method():
-#     if request.method == 'GET':
-#         # args_dict = request.args.to_dict()
-#         # print(args_dict)
-#         num = request.args["num"]
-#         name = request.args.get("name")
-#         return "GET으로 전달된 데이터({}, {})".format(num, name)
-#     else:
-#         num = request.form["num"]
-#         name = request.form["name"]
-#         return "POST로 전달된 데이터({}, {})".format(num, name)
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, render_template
 app = Flask(__name__)
 @app.route('/')
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from datetime import datetime
-
 import paho.mqtt.client as mqtt
 import firebase_admin
 from firebase_admin import firestore, credentials
 
 cred = credentials.Certificate("sungwonarduino-firebase-adminsdk-py0cm-ec2835c5c8.json")
 firebase_admin.initialize_app(cred)
-# Application Default credentials are automatically created.
 db = firestore.client()
-
-###########################################
-url = "http://127.0.0.1:5000/"
-
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -19,18 +13,14 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Bad connection Returned code=", rc)
 
-
 def on_disconnect(client, userdata, flags, rc=0):
     print(str(rc))
-
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print("subscribed: " + str(mid) + " " + str(granted_qos))
 
-
 def on_message(client, userdata, msg):
     print(str(msg.payload.decode("utf-8")))
-
     now = datetime.now()
     print(now.strftime('%Y-%m-%d %H:%M:%S'))
     db.collection('data').document('mat1').set({
@@ -70,28 +60,13 @@ def on_message(client, userdata, msg):
         'SensorTag': "6",
     })
 
-    # soundDB.update({'sound1': str(msg.payload.decode("utf-8"))})
-    # soundDB.update({'sound2': str(msg.payload.decode("utf-8"))})
-    # soundDB.update({'sound3': str(msg.payload.decode("utf-8"))})
-    # soundDB.update({'sound4': str(msg.payload.decode("utf-8"))})
-    # soundDB.update({'sound5': str(msg.payload.decode("utf-8"))})
-    # soundDB.update({'sound6': str(msg.payload.decode("utf-8"))})
-
-    # r = requests.post(url, data={'id': 'ksg', 'pw': 'password!@#'})
-    # print(r.status)
-    # print(r.text)
-
-
-# 새로운 클라이언트 생성
-client = mqtt.Client()
-# 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료), on_subscribe(topic 구독),
-# on_message(발행된 메세지가 들어왔을 때)
+client = mqtt.Client()  # 새로운 클라이언트 생성
 client.on_connect = on_connect
+# 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료),
+# on_subscribe(topic 구독), on_message(발행된 메세지가 들어왔을 때)
 client.on_disconnect = on_disconnect
 client.on_subscribe = on_subscribe
 client.on_message = on_message
-# address : localhost, port: 1883 에 연결
-client.connect('broker.mqttdashboard.com', 1883)
-# common topic 으로 메세지 발행
-client.subscribe('classTopic', 1)
+client.connect('broker.mqttdashboard.com', 1883) # address : localhost, port: 1883 에 연결
+client.subscribe('classTopic', 1) # classTopic 으로 메세지 발행
 client.loop_forever()
